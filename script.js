80af1c043f7337efbbb97b78eec7676b0e387aae
code-par-learn <117098183+code-par-learn@users.noreply.github.com>
2023-02-21 22:56:17 +0530

    created playround func

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("hi123");
+
+function getComputerChoice(){
+    let ch=["rock","paper","scissors"];
+    let rand=Math.floor(Math.random()*(3));
+    return ch[rand];
+} 
+function playround(playerSelection,ComputerSelection) {
+    let ps=playerSelection.toLowerCase();
+    let cs=ComputerSelection.toLowerCase();
+    let res;
+    if (ps===cs){
+        res="tie";
+        return 'its a tie';
+    }
+    else{
+        if(ps=="rock"){
+            res=(cs=="paper")?cs:ps;
+        }
+        else if(ps=="paper"){
+            res=(cs=="scissors")?cs:ps;
+        }
+        else{
+            res=(cs=="rock")?cs:ps;
+        }
+        
+    }
+    if (res==ps){
+        return `you Won! ${playerSelection} beats ${ComputerSelection}`;
+    }
+    else{
+        return `you Lost! ${ComputerSelection} beats ${playerSelection}`;
+    }
+    
+    
+}
+const player="paper";
+const computer=getComputerChoice();
+
