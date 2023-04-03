94f6365834f97fc6d5f6b471e4534a8979cfeacc
code-par-learn <117098183+code-par-learn@users.noreply.github.com>
2023-04-03 20:37:20 +0530

    new features added

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ function getComputerChoice(){
     return ch[rand];
 } 
 function playround(playerSelection,ComputerSelection) {
-    let ps=playerSelection.toLowerCase();
-    let cs=ComputerSelection.toLowerCase();
+    let ps=playerSelection;
+    let cs=ComputerSelection;
     let res;
     if (ps===cs){
         res="tie";
-        console.log('its a tie');
+        showres('its a tie');
         return res;
     }
     else{
@@ -22,42 +22,31 @@ function playround(playerSelection,ComputerSelection) {
         }
         else{
             res=(cs=="rock")?cs:ps;
-        }
-       
-        
+        }     
     }
     if (res==ps){
-        console.log(`you Won! ${playerSelection} beats ${ComputerSelection}`);
+        showres(`you Won! ${playerSelection} beats ${ComputerSelection}`);
         
     }
     else{
-        console.log(`you Lost! ${ComputerSelection} beats ${playerSelection}`);
+        showres(`you Lost! ${ComputerSelection} beats ${playerSelection}`);
         
     }
+
     return res;
     
     
 }
-function game(){
-    let yourscore=0;
-    let compscore=0;
-    for(let i=0;i<5;i++){
-        let player=prompt("your choice");
-        let pschoice=player.toLowerCase();
-        let error=(pschoice=="paper")?"no":(pschoice=="rock")?"no":(pschoice=="scissors")?"no":"yes";
-        
-        while (error=="yes"){
-            console.log("you entered the wrong choice");
-            player=prompt("your choice: ");
-            pschoice=player.toLowerCase();
-            error=(pschoice=="paper")?"no":(pschoice=="rock")?"no":(pschoice=="scissors")?"no":"yes";
-           
-        }
+let yourscore=0;
+let compscore=0;
+function game(ch){
+        let player=ch;
     
         let computer=getComputerChoice();
        
         let result=playround(player,computer);
-        if (result==player.toLowerCase()){
+        
+        if (result==player){
             yourscore+=1;
         }else if(result=="tie"){
             yourscore+=1;
@@ -66,11 +55,34 @@ function game(){
         else{
             compscore+=1;
         }
+    
+    showres(`your score ${yourscore}`);
+    showres(`computer score ${compscore}`);
+    
+   
+}
+function start(cho){
+    if(yourscore<5 && compscore<5){
+        game(cho);
     }
-    console.log(`your score ${yourscore}`);
-    console.log(`computer score ${compscore}`);
-    return (yourscore>compscore)?"you won computer lost":(yourscore<compscore)?"you lost computer won":"you both won";
-
+    else{
+    showres((yourscore>compscore)?"you won computer lost":(yourscore<compscore)?"you lost computer won":"you both won");
+    showres("Game Ended")
+    }
 }
 
+document.addEventListener('DOMContentLoaded', () => {
+const btn1=document.querySelectorAll('button');
 
+btn1.forEach((btn)=>{
+    btn.addEventListener('click',()=>{start(btn.value)});
+});
+});
+const container=document.querySelector('#container');
+
+
+function showres(res){
+    const pele=document.createElement('p');    
+    pele.textContent=`${res}`;
+    container.appendChild(pele); 
+}
